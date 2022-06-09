9638e0ca20ed258d41a32f62cd37d40801b2ab4f
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-06-10 00:25:39 +0430

    added range

--- shop/src/components/ItemsList/Item/Item.jsx
+++ shop/src/components/ItemsList/Item/Item.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import {Box, IconButton, Typography} from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-export default function Item (props) {
+
+export default function Item(props) {
     const {item} = props;
+    const [amount, setAmount] = React.useState(0);
     return (
-        <Box sx={{display:"flex", flexDirection:"column",margin:"0.5rem", width:"200px", border:"2px solid purple", borderRadius:"8px"}}>
-        <img src={item.image} alt={item.name}/>
-        <Box sx={{padding:"1rem"}}>
-            <Typography>{item.name}</Typography>
-            <Typography>{item.category}</Typography>
-            <Box sx={{display:"flex", justifyContent:"space-between", marginTop:"0.5rem"}}>
-            <Typography>{item.price}$</Typography>
-                <IconButton aria-label="buy">
-            <AddShoppingCartIcon sx={{cursor:"pointer"}}/>
-                </IconButton>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "0.5rem",
+            width: "200px",
+            border: "2px solid purple",
+            borderRadius: "8px",
+            padding:"0.2rem"
+        }}>
+                <img src={item.image} alt={item.name}/>
+            <Box sx={{padding: "1rem"}}>
+                <Typography>{item.name}</Typography>
+                <Typography>{item.category}</Typography>
+
+                <Box sx={{display: "flex", justifyContent: "space-between",alignItems:"center", marginTop: "0.5rem"}}>
+
+                    <Typography sx={{textDecoration:!item.storage ? "line-through" : "none"}}>{item.price}$</Typography>
+                    {!item.storage ? <Typography sx={{color:"red", fontSize:"1rem"}}>out of stock</Typography> : null}
+                    <IconButton aria-label="buy" disabled={!item.storage}>
+                        <AddShoppingCartIcon sx={{cursor: "pointer"}}/>
+                    </IconButton>
+                </Box>
             </Box>
         </Box>
-            </Box>
-        )}
+    )
+}
--- shop/src/components/ItemsList/ItemsList.jsx
+++ shop/src/components/ItemsList/ItemsList.jsx
@@ -1,28 +1,63 @@
 import React from 'react';
-import {Box, TextField, Typography} from "@mui/material";
+import {Box, Slider, TextField, Typography} from "@mui/material";
 import Item from "./Item/Item";
 
 export default function ItemsList(props) {
+
+
     const {items} = props;
     const [search, setSearch] = React.useState("");
 
+
+    function valuetext(value) {
+        return `${value}$`;
+    }
+
+    const minDistance = 5;
+
+    const [value1, setValue1] = React.useState([0, 200]);
+
+    const handleChangeRange = (event, newValue, activeThumb) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (activeThumb === 0) {
+            setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+        } else {
+            setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+        }
+    };
+
     return (
-        <Box sx={{display:"flex", flexDirection:"column", p:"4rem", width:{xs:"90%", md:"70%"}, gap:"2rem"}}>
+        <Box sx={{display: "flex", flexDirection: "column", p: "4rem", width: {xs: "90%", md: "70%"}, gap: "2rem"}}>
             <TextField id="search" label="search" variant="standard" onChange={event => setSearch(event.target.value)}/>
-        <Box sx={{display: "flex", flexWrap:"wrap"}}>
-            {
-                items.filter(post => {
-                    if (search === '') {
-                        return (
-                            <Typography>No items found</Typography>
-                        );
-                    } else if (post.name.toLowerCase().includes(search.toLowerCase())) {
-                        return post;
-                    }
-                }).map((post, index) => (
-                    <Item key={index} item={post}/>
-                ))
-            }
+            <Slider
+                getAriaLabel={() => 'Minimum distance'}
+                value={value1}
+                onChange={handleChangeRange}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+                min={10} max={200}
+                disableSwap
+            />
+            <Box sx={{display: "flex", flexWrap: "wrap"}}>
+                {
+
+                    items.filter(post => {
+                        if (search === '') {
+                            if (post.price >= value1[0] && post.price <= value1[1]) {
+                            return post}
+                        } else if (post.name.toLowerCase().includes(search.toLowerCase())) {
+                            if (post.price >= value1[0] && post.price <= value1[1]) {
+                                return post}
+                        }
+
+                    }).map((post, index) => (
+                        <Item key={index} item={post}/>
+                    ))
+                }
+            </Box>
         </Box>
-        </Box>
-            )}
+    );
+}
